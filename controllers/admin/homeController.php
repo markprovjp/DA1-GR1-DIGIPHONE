f278052db0eabe073decc6f8d2c07adba5427d4d
Nguyễn văn quyền <154734607+markprovjp@users.noreply.github.com>
2024-11-08 12:04:25 +0700

    Update homeController.php

--- controllers/admin/homeController.php
+++ controllers/admin/homeController.php
@@ -4,14 +4,3 @@ namespace controllers;
 
 
 
-class homeController
-{
-    public $url = BASE_URL;
-
-    public function index()
-    {
-        require_once 'views/site/home/index.php';
-    }
-
-    
-}
